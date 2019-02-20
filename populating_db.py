86ea7138a2d52f2caf0c3f8745ace948c0795e5e
Quentin Bertrand <quentin.bertrand@yahoo.fr>
2019-02-20 18:34:19 +0100

    compte_employe table done. 10/15

--- populating_db.py
+++ populating_db.py
@@ -14,13 +14,14 @@ INGREDIENTS = "Coulis de tomate,Tabasco,Baparella,Oignons,Poivrons,Poulet,Crème
               "Crevettes,Persillade,Citron,Cheddar,Reblochon,Miel,Oeuf,Thon,Moutarde,Tomates,Cornichons," \
               "Lardons".split(",")
 
+PIZZERIAS_OWNER = 'OC Pizza'
 NUMBER_OF_PIZZERIAS = 5
 
 fake = Faker('fr_FR')
 
 
 def clients_creation(number):
-    fake_clients = [{'nom': 'OC Pizza', 'prenom': 'OC Pizza', 'mail': 'openclassrooms@projet6.com'}]
+    fake_clients = [{'nom': PIZZERIAS_OWNER, 'prenom': PIZZERIAS_OWNER, 'mail': fake.email()}]
 
     for _ in range(1, number):
         fake_clients.append({'nom': fake.last_name(), 'prenom': fake.first_name(), 'mail': fake.email()})
@@ -56,13 +57,13 @@ def pizzas_creation(number):
 
 def type_employe_creation(number):
     fake_type_employes = []
-    unique_job = []
 
     while len(fake_type_employes) < number:
         job = fake.job().strip()
-        if job not in unique_job and len(job) < 50:
-            unique_job.append(job)
-            fake_type_employes.append({'nom_type': job})
+        type_employe = {'nom_type': job}
+
+        if type_employe not in fake_type_employes and len(job) < 50:
+            fake_type_employes.append(type_employe)
         else:
             pass
 
@@ -83,7 +84,7 @@ def factures_creation(number):
 def adresses_creation(db, number):
     fake_adresses = []
     client_ids = _get_table_ids(db, 'client')[1:]
-    oc_pizza_id = db.query("SELECT * FROM client WHERE nom = 'OC Pizza'")[0]['id']
+    oc_pizza_id = db.query(f"SELECT * FROM client WHERE nom = '{PIZZERIAS_OWNER}'")[0]['id']
 
     for _ in range(0, NUMBER_OF_PIZZERIAS):
         fake_adresses.append({'client_id': oc_pizza_id,
@@ -119,7 +120,7 @@ def recettes_creation(db):
 
 def pizzerias_creation(db):
     fake_pizzerias = []
-    oc_client_id = db.query("SELECT * FROM client WHERE nom = 'OC Pizza'")[0].id
+    oc_client_id = db.query(f"SELECT * FROM client WHERE nom = '{PIZZERIAS_OWNER}'")[0].id
     oc_adresses = db.query(f"SELECT * FROM adresse_livraison WHERE client_id = {oc_client_id}")
 
     oc_adresse_ids = [row.id for row in oc_adresses]
@@ -133,12 +134,37 @@ def pizzerias_creation(db):
     return fake_pizzerias
 
 
-def _get_table_ids(db, table):
-    ids = []
-    rows = db.query(f'SELECT * from {table}')
+def employe_creation(db):
+    fake_employes = []
+    type_employe_ids = _get_table_ids(db, 'type_employe')
 
-    for row in rows:
-        ids.append(row['id'])
+    for pizzeria_id in range(1, NUMBER_OF_PIZZERIAS + 1):
+        for _ in range(0, random.randint(5, 10)):
+            fake_employes.append({'nom': fake.last_name(),
+                                  'prenom': fake.first_name(),
+                                  'pizzeria_id': pizzeria_id,
+                                  'type_employe_id': random.choice(type_employe_ids)
+                                  })
+
+    return fake_employes
+
+
+def compte_employe_creation(db):
+    fake_accounts = []
+    employe_ids = _get_table_ids(db, 'employe')
+
+    for id in employe_ids:
+        fake_accounts.append({'employe_id': id,
+                              'username': fake.user_name(),
+                              'password': fake.password()
+                              })
+
+    return fake_accounts
+
+
+def _get_table_ids(db, table):
+    rows = db.query(f'SELECT * from {table}')
+    ids = [row.id for row in rows]
 
     return ids
 
@@ -146,30 +172,38 @@ def _get_table_ids(db, table):
 def main():
     db = records.Database(f'postgres://{USERNAME}:{PASSWORD}@localhost/{DB_NAME}')
 
-    # fake_clients = clients_creation(10)
-    # db.bulk_query('INSERT INTO client (nom, prenom, mail) VALUES (:nom, :prenom, :mail)', fake_clients)
-    #
-    # fake_ingredients = ingredients_creation(10)
-    # db.bulk_query('INSERT INTO ingredient (nom) VALUES (:nom)', fake_ingredients)
-    #
-    # fake_pizzas = pizzas_creation(10)
-    # db.bulk_query('INSERT INTO pizza (nom, prix) VALUES (:nom, :prix)', fake_pizzas)
-    #
-    # fake_type_employe = type_employe_creation(10)
-    # db.bulk_query('INSERT INTO type_employe (nom_type) VALUES (:nom_type)', fake_type_employe)
-    #
-    # fake_factures = factures_creation(10)
-    # db.bulk_query('INSERT INTO facture (date, payee, lien) VALUES (:date, :payee, :lien)', fake_factures)
-    #
-    # fake_adresses = adresses_creation(db, 10)
-    # db.bulk_query('INSERT INTO adresse_livraison (client_id, code_postal, ville, rue, numero_habitation)'
-    #               'VALUES (:client_id, :code_postal, :ville, :rue, :numero_habitation)', fake_adresses)
-    #
-    # fake_recettes = recettes_creation(db)
-    # db.bulk_query('INSERT INTO recette (recette, pizza_id) VALUES (:recette, :pizza_id)', fake_recettes)
-    #
-    # fake_pizzerias = pizzerias_creation(db)
-    # db.bulk_query('INSERT INTO pizzeria (nom, adresse_id) VALUES (:nom, :adresse_id)', fake_pizzerias)
+    fake_clients = clients_creation(10)
+    db.bulk_query('INSERT INTO client (nom, prenom, mail) VALUES (:nom, :prenom, :mail)', fake_clients)
+
+    fake_ingredients = ingredients_creation(10)
+    db.bulk_query('INSERT INTO ingredient (nom) VALUES (:nom)', fake_ingredients)
+
+    fake_pizzas = pizzas_creation(10)
+    db.bulk_query('INSERT INTO pizza (nom, prix) VALUES (:nom, :prix)', fake_pizzas)
+
+    fake_type_employe = type_employe_creation(5)
+    db.bulk_query('INSERT INTO type_employe (nom_type) VALUES (:nom_type)', fake_type_employe)
+
+    fake_factures = factures_creation(10)
+    db.bulk_query('INSERT INTO facture (date, payee, lien) VALUES (:date, :payee, :lien)', fake_factures)
+
+    fake_adresses = adresses_creation(db, 10)
+    db.bulk_query('INSERT INTO adresse_livraison (client_id, code_postal, ville, rue, numero_habitation)'
+                  'VALUES (:client_id, :code_postal, :ville, :rue, :numero_habitation)', fake_adresses)
+
+    fake_recettes = recettes_creation(db)
+    db.bulk_query('INSERT INTO recette (recette, pizza_id) VALUES (:recette, :pizza_id)', fake_recettes)
+
+    fake_pizzerias = pizzerias_creation(db)
+    db.bulk_query('INSERT INTO pizzeria (nom, adresse_id) VALUES (:nom, :adresse_id)', fake_pizzerias)
+
+    fake_employes = employe_creation(db)
+    db.bulk_query('INSERT INTO employe (nom, prenom, pizzeria_id, type_employe_id) '
+                  'VALUES (:nom, :prenom, :pizzeria_id, :type_employe_id)', fake_employes)
+
+    fake_compte_employes = compte_employe_creation(db)
+    db.bulk_query('INSERT INTO compte_employe (employe_id, username, password)'
+                  'VALUES (:employe_id, :username, :password)', fake_compte_employes)
 
 
 if __name__ == '__main__':
